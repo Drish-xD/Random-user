824f88213e46100ea75cd44c5c7843a25188d3c2
Drish-xD <drish.xd@gmail.com>
2021-09-26 14:48:25 +0530

    Uploaded demo

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const app = Vue.createApp({
-    template: '<h1>Hello World</h1>',
+    data () {
+        return {
+            FName: 'Lorem',
+            LName: 'Ipsuim',
+            Email: 'jbcdhnc@gmail.com',
+            Phone: '88656559566',
+            Address: 'lorem ipsium',
+            Picture: 'https://randomuser.me/api/portraits/men/67.jpg',
+        }
+    },
 })
 
 app.mount('#app')
