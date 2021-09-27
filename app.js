3d1d6dbef4da2667d51e2e659ae9f9a0afe931bf
Drish-xD <Drish.xD@gmail.com>
2021-09-27 12:19:17 +0530

    Update 2.0

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const app = Vue.createApp({
-    data () {
+    data() {
         return {
             Title: 'Random User Generator',
             FName: 'Lorem',
@@ -9,6 +9,20 @@ const app = Vue.createApp({
             Address: 'lorem ipsium',
             Picture: 'https://randomuser.me/api/portraits/lego/5.jpg',
         }
+    },
+    methods: {
+        async getUser() {
+            const res = await fetch('https://randomuser.me/api')
+            const { results } = await res.json()
+
+            this.FName = results[0].name.first
+            this.FName = results[0].name.last
+            this.Email = results[0].email
+            this.Phone = results[0].phone
+            this.Address = results[0].location.country
+            this.Picture = results[0].picture.large
+            
+        },
     },
 })
 
